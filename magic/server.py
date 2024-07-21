4a5fab3e69208a99deef05c06edde45fa2f69d4e
k1-k0 <kmssrvk@gmail.com>
2024-07-21 18:43:04 +0300

    update gitignore

--- magic/server.py
+++ magic/server.py
@@ -1,8 +1,24 @@
+from enum import StrEnum
 import logging
 from typing import NoReturn
-from aiohttp.web import Application, Request, Response, get, run_app, WebSocketResponse
+from aiohttp.web import (
+    Application,
+    Request,
+    Response,
+    get,
+    run_app,
+    WebSocketResponse,
+    StreamResponse,
+)
 from aiohttp import WSMsgType
 from faker import Faker
+
+
+# TODO: перенести в consts модуль
+class Action(StrEnum):
+    JOIN = "join"
+    SENT = "sent"
+    READY = "ready"
 
 
 log = logging.getLogger(__name__)
@@ -17,61 +33,72 @@ async def hello(request: Request) -> Response:
     return Response(text="Hello, world!")
 
 
-async def websocket_handler(request: Request):
+async def websocket_handler(request: Request) -> StreamResponse:
     ws_current = WebSocketResponse()
+
     ws_ready = ws_current.can_prepare(request)
     if not ws_ready.ok:
         return Response(text="not ready yet")
 
     await ws_current.prepare(request)
 
-    name = request.query.get("name")
-    log.info('%s joined.', name)
+    name = request.query.get("name", get_random_name())
+    log.info("%s joined.", name)
 
-    await ws_current.send_json({'action': 'connect', 'name': name})
+    await ws_current.send_json({"action": "connect", "name": name})
 
-    for ws in request.app['websockets'].values():
-        await ws.send_json({'action': 'join', 'name': name})
+    for ws in request.app["websockets"].values():
+        await ws.send_json({"action": "join", "name": name})
 
     # NOTE: name - websocket
-    request.app['websockets'][name] = ws_current
+    request.app["websockets"][name] = ws_current
 
     while True:
         msg = await ws_current.receive()
 
         if msg.type == WSMsgType.text:
-            for ws in request.app['websockets'].values():
+            for ws in request.app["websockets"].values():
                 if ws is not ws_current:
-                    await ws.send_json({'action': 'sent', 'name': name, 'text': msg.data})
+                    await ws.send_json(
+                        {
+                            "action": "sent",
+                            "name": name,
+                            "text": msg.data,
+                        },
+                    )
         else:
             break
 
-    del request.app['websockets'][name]
-    log.info('%s disconnected.', name)
-    for ws in request.app['websockets'].values():
-        await ws.send_json({'action': 'disconnect', 'name': name})
+    del request.app["websockets"][name]
+    log.info("%s disconnected.", name)
+    for ws in request.app["websockets"].values():
+        await ws.send_json({"action": "disconnect", "name": name})
 
     return ws_current
 
 
 async def clear_websockets(app: Application) -> None:
-    for websocket in app['websockets'].values():
+    for websocket in app["websockets"].values():
         await websocket.close()
 
-    app['websockets'].clear()
+    app["websockets"].clear()
 
 
+# TODO: создание app в отдельный модуль
 def main() -> NoReturn:
     app = Application()
 
     app.on_shutdown.append(clear_websockets)
 
-    app.add_routes(routes=[
-        get("/", hello),
-        get("/ws", websocket_handler),
-    ])
+    app.add_routes(
+        routes=[
+            # TODO: handlers в отдельный модуль
+            get("/", hello),
+            get("/ws", websocket_handler),
+        ]
+    )
 
-    app['websockets'] = {}
+    app["websockets"] = {}
 
     run_app(app=app)
 
